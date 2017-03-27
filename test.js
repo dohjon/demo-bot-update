d5c25bc76349babae437fb63c8ae156ac7721434
Jonathan Doherty <jonathan.doherty.work@outlook.com>
2017-03-27 21:57:16 +0200

    Tweak

--- test.js
+++ test.js
@@ -1,3 +1,22 @@
 'use strict';
 
 import test from 'ava';
+import {updateAvailable, applyUpdate} from './';
+
+test(`exports updateAvailable`, t => {
+	t.not(typeof updateAvailable, 'undefined');
+	t.is(typeof updateAvailable, 'function');
+});
+
+test(`calling updateAvailable should return a promise`, t => {
+	t.is(typeof updateAvailable().then, 'function');
+});
+
+test(`exports applyUpdate`, t => {
+	t.not(typeof applyUpdate, 'undefined');
+	t.is(typeof applyUpdate, 'function');
+});
+
+test(`calling applyUpdate should return a promise`, t => {
+	t.is(typeof applyUpdate().then, 'function');
+});
