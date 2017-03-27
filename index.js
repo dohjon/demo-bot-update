215b52c3c381ab00d308096e06bee9820a6f6f1b
Jonathan Doherty <jonathan.doherty.work@outlook.com>
2017-03-27 17:22:46 +0200

    Tweak

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
 
 const debug = require('debug')('demo-bot-update');
+
+// 1. find the absolute path to the executing cli
+
+// 2. check if the remote git repository is ahead of the local git repository
+
+// 3. prompt user to update
+
